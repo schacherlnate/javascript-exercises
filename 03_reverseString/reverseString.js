67d5902b9cf94ae3c8d863df13ce20fb1dd3d291
schacherlnate <139157234+schacherlnate@users.noreply.github.com>
2025-02-06 17:54:47 -0600

    finish ex 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,14 @@
-const reverseString = function() {
+const reverseString = function(str) {
 
+    let revStr = '';
+    const strLen = str.length;
+
+    for (let i = 0; i < str.length; i++) {
+        let strChar = str[str.length-i-1];
+        revStr = revStr.concat(strChar);
+    }
+
+    return revStr
 };
 
 // Do not edit below this line
