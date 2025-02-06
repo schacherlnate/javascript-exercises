e2d4dfecdfaa2709ff97d5ea524e4df21d118268
schacherlnate <139157234+schacherlnate@users.noreply.github.com>
2025-02-06 17:46:56 -0600

    finish ex 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(str, numRep) {
+    if (numRep < 0) {
+        return 'ERROR';
+    }
+    
+    let i = 0;
+    let repStr = "";
+    while (i < numRep) {
+        repStr = repStr.concat(str);
+        i++;
+    }
+    return repStr;
 };
 
 // Do not edit below this line
