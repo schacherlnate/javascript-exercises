bfeba031733ae8123f093bcd8ce422f5b482ac36
schacherlnate <139157234+schacherlnate@users.noreply.github.com>
2025-02-06 20:56:34 -0600

    finish ex 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let sum = 0;
 
+    // Make sure that our inputs are positive integers
+    if (typeof(num1)!='number' || typeof(num2)!='number' ||
+                    !Number.isInteger(num1) || !Number.isInteger(num2) ||
+                    num1 < 0 || num2 < 0) {
+        return 'ERROR';
+    }
+
+    // We want to sum for the range regardless of input order
+    let smallerNum = Math.min(num1, num2);
+    let biggerNum = Math.max(num1, num2);
+
+    for (let i = smallerNum; i<=biggerNum; i++) {
+        sum+=i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
