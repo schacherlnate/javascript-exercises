424813d5b4268abb2c3bdac3480b6ba3541d5e4c
schacherlnate <139157234+schacherlnate@users.noreply.github.com>
2025-02-06 21:03:55 -0600

    finish ex 6

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,10 @@
-const leapYears = function() {
+const leapYears = function(year) {
 
+    divByFour = year%4 == 0;
+    divByHundred = year%100 == 0;
+    divByFourHundred = year%400 == 0;
+    
+    return (divByFour && (!divByHundred || divByFourHundred)) 
 };
 
 // Do not edit below this line
