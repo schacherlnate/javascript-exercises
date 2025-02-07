a1ac7deea26c19f1030d03839edf8b1a5cf45d92
schacherlnate <139157234+schacherlnate@users.noreply.github.com>
2025-02-06 20:40:32 -0600

    finish ex 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,22 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...remTheseVals) {
+
+    const returnArr = [];
+
+    for (let i = 0; i < arr.length; i++) {
+        let testNum = arr[i];
+        let isGood = true;
+        for (let j = 0; j < remTheseVals.length; j++) {
+            let badNum = remTheseVals[j];
+            if (testNum===badNum) {
+                isGood = false;
+                break;
+            }
+        }
+        if (isGood) {
+            returnArr.push(testNum);
+        }
+    }
+    return returnArr
 };
 
 // Do not edit below this line
