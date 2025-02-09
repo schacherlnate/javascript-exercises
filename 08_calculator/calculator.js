86cf7059f6398ab3a9e7e087f585d8c2d34877dc
schacherlnate <139157234+schacherlnate@users.noreply.github.com>
2025-02-09 15:02:26 -0600

    finish ex 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,31 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a-b
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((s, num)=>{
+    return s+num
+  }, 0)
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((p, num)=>p*num, 1)
 };
 
-const power = function() {
-	
+const power = function(b, x) {
+	return b**x
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+	if (n==0) {
+    return 1
+  }
+
+  return n*factorial(n-1)
 };
 
 // Do not edit below this line
