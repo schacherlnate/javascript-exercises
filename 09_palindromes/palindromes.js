d773387f8ad327d2830dc3bd225c6b9195797d06
schacherlnate <139157234+schacherlnate@users.noreply.github.com>
2025-02-09 15:32:07 -0600

    finish ex 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,23 @@
-const palindromes = function () {
-
+const isCharValid = function(char) {
+    return (char >= "a" && char <= "z") || (char>="0" && char<="9");
+}
+const palindromes = function (str) {
+    let i = 0;
+    let j = str.length-1;
+    const strLower = str.toLowerCase();
+    while (j>i) {
+        if (!isCharValid(strLower[i])) {
+            i++;
+        } else if (!isCharValid(strLower[j])) {
+            j--;
+        } else if (strLower[i]!=strLower[j]) {
+            return false;
+        } else {
+            i++;
+            j--;
+        }
+    }
+    return true;
 };
 
 // Do not edit below this line
