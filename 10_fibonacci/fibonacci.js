ec634c8546a70f812f285366351b72644f4c2a68
schacherlnate <139157234+schacherlnate@users.noreply.github.com>
2025-02-09 21:53:36 -0600

    finish ex 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,8 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    let res = (n < 0)? "OOPS":
+                (n==0) ? 0:
+                    (n <= 2) ? 1: fibonacci(n-1) + fibonacci(n-2);
+    return res
 };
 
 // Do not edit below this line
