e72fb97d04d14fc4daf9f101b98b0ac24d63db4d
schacherlnate <139157234+schacherlnate@users.noreply.github.com>
2025-02-09 22:08:36 -0600

    finish ex 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    people.forEach(person=>{
+        if (!("yearOfDeath" in person)) {
+            person.yearOfDeath = 2025;
+        }
+    })
+    let oldestPerson = people.sort((p1, p2)=>{
+        let p1age = p1.yearOfDeath-p1.yearOfBirth;
+        let p2age = p2.yearOfDeath-p2.yearOfBirth;
+        return (p1age - p2age)
+    }).at(-1);
+    return oldestPerson
 };
 
 // Do not edit below this line
